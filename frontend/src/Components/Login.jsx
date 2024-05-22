438a15d4c967e4cbf5d0ca69258db96cb7321c6e
PuneetChopra197 <158151334+PuneetChopra197@users.noreply.github.com>
2024-05-22 12:04:56 +0530

    Live

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
     }
     if (Object.keys(error).length === 0) {
       const res = await axios.post(
-        "http://localhost:5000/auth/login",
+        "https://social-blog-1.onrender.com/auth/login",
         formData
       );
 
